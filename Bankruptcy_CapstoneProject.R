5971f57cf9b936a27eb71b7599e008af4796e679
Daniel Handojo <d.handojo@outlook.de>
2021-04-08 10:13:27 +0200

    Bankcruptcy Capstone

---
+++ Bankruptcy_CapstoneProject.R
@@ -0,0 +1,17 @@
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+if(!require(lubridate)) install.packages("lubridate", repos = "http://cran.us.r-project.org")
+if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+if(!require(RColorBrewer)) install.packages("RColorBrewer", repos ="http://cran.us.r-project.org")
+
+library(tidyverse)
+library(caret)
+library(data.table)
+library(lubridate)
+library(ggplot2)
+library(RColorBrewer)
+options(digits=3)
+
+
+
